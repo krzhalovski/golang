cef64cce9e9acee2f40c43d2d897f7589c151625
Danail Krzhalovski <danailkrzhalovski@localhost.localdomain>
2019-06-18 18:09:46 +0200

    Finished Chapter 1

---
+++ dkrzhalovski/go_book/CH01_Tutorial/lissajous/lissa.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.Black,
+	color.RGBA{0xFF, 0, 0, 0xFF},       // red
+	color.RGBA{0xFF, 0xA5, 0, 0xFF},    // orange
+	color.RGBA{0xFF, 0xFF, 0, 0xFF},    // yellow
+	color.RGBA{0, 0xFF, 0, 0xFF},       // green
+	color.RGBA{0, 0, 0xFF, 0xFF},       // blue
+	color.RGBA{0x4B, 0, 0x82, 0xFF},    // indigo
+	color.RGBA{0xEE, 0x82, 0xEE, 0xFF}, // violet
+}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64()
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	var index uint8
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		if index == 8 {
+			index = 0
+		} else {
+			index++
+		}
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
+		}
+
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	gif.EncodeAll(out, &anim)
+}
